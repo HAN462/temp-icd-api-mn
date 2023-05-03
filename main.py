fcff475b9de2200f948be1adab479f90ae467414
HAN462 <vicky.chen.1@stonybrook.edu>
2023-05-03 21:42:20 +0000

    updated main.py

--- main.py
+++ main.py
@@ -17,26 +17,3 @@ def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
-
-    @app.route('/services/<value>', methods=['GET'])
-def services(value):
-    print('value: ', value)
-    filtered = df[df['svc_code'] == value]
-    if len(filtered) <= 0:
-        return 'There is nothing here'
-    else: 
-        return filtered.to_json(orient="records")
-
-@app.route('/services/<value>/svc_code_desc/<value2>')
-def services2(value, value2):
-    filtered = df[df['svc_code'] == value]
-    filtered2 = filtered[filtered['svc_code_desc'] == value2]
-    if len(filtered2) <= 0:
-        return 'There is nothing here'
-    else: 
-        return filtered2.to_json(orient="records")    
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
-

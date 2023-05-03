a92ddd03877505eb9a105f669d6103be7cb41774
HAN462 <vicky.chen.1@stonybrook.edu>
2023-05-03 22:34:22 +0000

    updated main.py

--- main.py
+++ main.py
@@ -3,21 +3,3 @@ import pandas as pd
 
 app = FastAPI()
 df = pd.read_csv('./data/services2019.csv')
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.route('/', methods=["GET"])
-def home():
-    return 'this is a API service for MN services details'
-
-@app.route('/preview', methods=["GET"])
-def preview():
-    top10rows = df.head(1)
-    result= top10rows.to_json(orient="records")
-    return result  
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
